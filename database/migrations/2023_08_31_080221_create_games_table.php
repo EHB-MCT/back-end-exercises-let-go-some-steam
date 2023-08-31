85d305800028e7ac70768816683b138b96e4b820
Bert Heyman <bert.heyman@hotmail.com>
2023-08-31 10:26:11 +0200

    Name for games

--- database/migrations/2023_08_31_080221_create_games_table.php
+++ database/migrations/2023_08_31_080221_create_games_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->nullable();
+            $table->string('name')->nullable();
             $table->text('description')->nullable();
             $table->dateTime('release_date')->nullable();
             $table->integer('pegi_min_age')->nullable();
